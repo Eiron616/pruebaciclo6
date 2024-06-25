f855e16040d53c68897fffff23a2dc4edad0444a
Eiron616 <aaron_fr999@hotmail.com>
2024-06-24 21:20:14 -0500

    primera version

---
+++ src/main/java/com/cibertec/Dawii20211Grupo06Domingo03Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawii20211Grupo06Domingo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawii20211Grupo06Domingo03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/Dawii20211Grupo06Domingo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dawii20211Grupo06Domingo03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
