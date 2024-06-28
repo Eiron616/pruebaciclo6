5e07c5dbdbcb6948f17ceb43575ca6c05cfb8e07
Eiron616 <aaron_fr999@hotmail.com>
2024-06-27 21:16:09 -0500

    Controlador Usuario

---
+++ src/main/java/com/cibertec/controller/RegistroUsuarioController.java
@@ -0,0 +1,15 @@
+package com.cibertec.controller;
+
+/**
+ * @author Aaron Figueroa
+ */
+
+
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class RegistroUsuarioController {
+
+}
